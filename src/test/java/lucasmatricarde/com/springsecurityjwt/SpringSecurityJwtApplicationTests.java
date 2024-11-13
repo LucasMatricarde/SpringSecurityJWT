9656a9a88e089c5549b970539904281de97bc526
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-11-12 21:51:41 -0300

    Primeiro commit

---
+++ src/test/java/lucasmatricarde/com/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package lucasmatricarde.com.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
